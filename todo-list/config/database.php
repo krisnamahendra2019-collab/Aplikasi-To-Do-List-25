2ce412d80b4a9ebff78579b327edd7c8be6ed01f
Alit.4rya <alitbaim2@gmail.com>
2025-12-20 17:52:04 +0800

    Update database.php

---
+++ todo-list/config/database.php
@@ -0,0 +1,15 @@
+<?php
+// Konfigurasi database
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "todo_db";
+
+// Membuat koneksi
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+// Cek koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
